c0552d7df779599c8a5f46202a3af3af96847355
Flash2oo2 <sumitnishad2002@gmail.com>
2024-01-08 00:09:56 +0530

    First commit

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 import pickle
 
-cv = pickle.load(open("models/cv.pkl", 'rb'))
-clf = pickle.load(open("models/clf.pkl", 'rb'))
+cv = pickle.load(open("models/cv.pkl", "rb"))
+clf = pickle.load(open("models/clf.pkl", "rb"))
 
-def model_predict(email):
-    if email == "":
+
+def model_predict(txt_msg):
+    if txt_msg == "":
         return ""
-    tokenized_email = cv.transform([email]) # X 
-    prediction = clf.predict(tokenized_email)
-    # If the email is spam prediction should be 1
+    tokenized_msg = cv.transform([txt_msg])  # X
+    prediction = clf.predict(tokenized_msg)
+
     prediction = 1 if prediction == 1 else -1
     return prediction
